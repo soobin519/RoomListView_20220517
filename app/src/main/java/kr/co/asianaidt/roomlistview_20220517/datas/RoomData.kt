ef02ec8bab3655129b5662eef33d1406a5b9d660
soobin519 <soobin0519@naver.com>
2022-05-17 15:50:21 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/asianaidt/roomlistview_20220517/datas/RoomData.kt
@@ -0,0 +1,15 @@
+package kr.co.asianaidt.roomlistview_20220517.datas
+
+class RoomData(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val explain: String) {
+
+    //층수를 구하는 함수
+//    fun getFloor(): String {
+//        var newFloor = 4
+//
+//        return newFloor
+    }
+}
