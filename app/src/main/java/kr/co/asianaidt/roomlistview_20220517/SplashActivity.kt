f3193de685afa71da132e9cb2609cf292853d5b5
soobin519 <soobin0519@naver.com>
2022-05-18 10:20:39 +0900

    2초 지연 처리

--- app/src/main/java/kr/co/asianaidt/roomlistview_20220517/SplashActivity.kt
+++ app/src/main/java/kr/co/asianaidt/roomlistview_20220517/SplashActivity.kt
@@ -1,11 +1,28 @@
 package kr.co.asianaidt.roomlistview_20220517
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //2초 정도 후에 (지연 시킨 후에) 화면 이동
+        val myHandler = Handler( Looper.getMainLooper())
+
+        myHandler.postDelayed( {
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+
+        }, 2000)
+
+
     }
 }
